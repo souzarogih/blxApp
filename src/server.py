813029e5493caf437e42ddfad8d1585357a9531d
HigorSouza <higor.andrade@hotmail.com>
2023-03-28 22:13:47 -0300

    Primeiros códigos do projeto

---
+++ src/server.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Depends, status
+from src.schemas.schemas import Produto, ProdutoResponse
+from sqlalchemy.orm import Session
+from typing import List
+from src.infra.sqlalchemy.config.database import get_db, criar_db
+from src.infra.sqlalchemy.repositorios.produto import RepositorioProduto
+
+criar_db()
+
+app = FastAPI()
+
+
+@app.post('/produtos', status_code=status.HTTP_201_CREATED, response_model=ProdutoResponse)
+def criar_produto(produto: Produto, db: Session = Depends(get_db)):
+    produto_criado = RepositorioProduto(db).criar(produto)
+    return produto_criado
+
+
+@app.get('/produtos', status_code=status.HTTP_200_OK, response_model=List[Produto])
+def listar_produtos(db: Session = Depends(get_db)):
+    produtos = RepositorioProduto(db).listar()
+    return produtos
+
+
+# uvicorn src.server:app --reload --reload-dir=src
+# https://github.com/rogeriosilva-ifpi/ifpi-tds-20211-backend/blob/main/blx-backend/src/infra/sqlalchemy/config/database.py
